7ebf59793e0af8c542b3e5daa000d64264646f62
Suven <imsuven@outlook.com>
2021-06-11 01:31:27 +0800

    Update comments

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -1,16 +1,28 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
-  
-  document$.subscribe(() => {
-    MathJax.typesetPromise()
-  })
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
+
+const gitalk = new Gitalk({
+  clientID: '192844eda51bfad64420',
+  clientSecret: 'af922effc6dc62de3fedb61169b7b4935c673c1e',
+  repo: 'ai-edu-preview',      // The repository of store comments,
+  owner: 'imsuven',
+  admin: ['imsuven'],
+  id: location.pathname,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+})
+
+gitalk.render('gitalk-container')
