1649f842d7efc9b50d44f9484a67e7b9d764d484
Suven <imsuven@outlook.com>
2021-06-11 02:14:43 +0800

    update mkdocs

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -11,20 +11,15 @@ window.MathJax = {
   }
 };
 
-document$.subscribe(() => {
-  MathJax.typesetPromise()
-})
-
 var gitalk = new Gitalk({
   clientID: '192844eda51bfad64420',
   clientSecret: 'af922effc6dc62de3fedb61169b7b4935c673c1e',
   repo: 'ai-edu-preview',      // The repository of store comments,
   owner: 'imsuven',
   admin: ['imsuven'],
-  id: location.pathname,      // Ensure uniqueness and length less than 50
+  id: document.title,      // Ensure uniqueness and length less than 50
   distractionFreeMode: false,  // Facebook-like distraction free mode
   title: document.title,
-  labels: ['Gitalk', document.title],
   body: location.href + document.getElementsByTagName("meta")["description"]
 })
 
