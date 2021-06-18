d1e22025ce65832492eca3e711f78bfff52a6352
Suven <imsuven@outlook.com>
2021-06-18 11:59:21 +0800

    fix clarity

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -24,9 +24,3 @@ var gitalk = new Gitalk({
 })
 
 gitalk.render('gitalk-container')
-
-  (function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-  })(window, document, "clarity", "script", "76j9m09jeq");
