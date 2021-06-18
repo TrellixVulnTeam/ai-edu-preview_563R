189cc4e3c50fd0fe6de41d6a09519a2750768924
Suven <imsuven@outlook.com>
2021-06-18 12:07:48 +0800

    fix clarity

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -21,6 +21,12 @@ var gitalk = new Gitalk({
   distractionFreeMode: false,  // Facebook-like distraction free mode
   title: document.title,
   body: location.href + document.getElementsByTagName("meta")["description"]
-})
+});
 
-gitalk.render('gitalk-container')
+gitalk.render('gitalk-container');
+
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "76j9m09jeq");
