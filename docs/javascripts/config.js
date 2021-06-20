805f5aeef88a6502915fba99d58a9227643c7b60
Suven <imsuven@outlook.com>
2021-06-20 13:45:36 +0800

    fix mathjax

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -1,38 +1,23 @@
-(function () {
-    'use strict';
-    
-    var katexMath = (function () {
-        var maths = document.querySelectorAll('.arithmatex'),
-            tex;
-    
-        for (var i = 0; i < maths.length; i++) {
-          tex = maths[i].textContent || maths[i].innerText;
-          if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
-            katex.render(tex.slice(2, -2), maths[i], {'displayMode': false});
-          } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
-            katex.render(tex.slice(2, -2), maths[i], {'displayMode': true});
-          }
-        }
-    });
-    
-    (function () {
-      var onReady = function onReady(fn) {
-        if (document.addEventListener) {
-          document.addEventListener("DOMContentLoaded", fn);
-        } else {
-          document.attachEvent("onreadystatechange", function () {
-            if (document.readyState === "interactive") {
-              fn();
+window.MathJax = {
+    options: {
+      ignoreHtmlClass: 'tex2jax_ignore',
+      processHtmlClass: 'tex2jax_process',
+      renderActions: {
+        find: [10, function (doc) {
+          for (const node of document.querySelectorAll('script[type^="math/tex"]')) {
+            const display = !!node.type.match(/; *mode=display/);
+            const math = new doc.options.MathItem(node.textContent, doc.inputJax[0], display);
+            const text = document.createTextNode('');
+            const sibling = node.previousElementSibling;
+            node.parentNode.replaceChild(text, node);
+            math.start = {node: text, delim: '', n: 0};
+            math.end = {node: text, delim: '', n: 0};
+            doc.math.push(math);
+            if (sibling && sibling.matches('.MathJax_Preview')) {
+              sibling.parentNode.removeChild(sibling);
             }
-          });
-        }
-      };
-    
-      onReady(function () {
-        if (typeof katex !== "undefined") {
-          katexMath();
-        }
-      });
-    })();
-    
-    }());
+          }
+        }, '']
+      }
+    }
+  };
